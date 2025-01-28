986ba3f9686fa43c4da659e65b1be8704163b253
Eduardollpz <angel.lopez_130@hotmail.com>
2025-01-28 11:42:27 -0600

    1

---
+++ src/eduardo/actividades/actividad2/leetcode/Problema1.java
@@ -0,0 +1,20 @@
+package eduardo.actividades.actividad2.leetcode;
+
+public class Problema1 {
+
+    public static void main(String[] args) {
+        String cadena = "Hola mundo";
+        String cadenaBuena = "";
+        int i = cadena.length();
+
+        while(i!= 0){
+            cadenaBuena +=""+cadena.charAt(i-1);
+            i--;
+        }
+        System.out.println(cadenaBuena);
+
+    }
+
+
+
+}
